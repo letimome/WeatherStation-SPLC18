0de05e185c04e83c8a8e653261f6c4b086eca341
letimome <leticia.montalvillo@ehu.es>
2018-03-23 20:49:59 +0100

    windirection measure #6

---
+++ output/London/js/settings.js
@@ -0,0 +1,28 @@
+
+// 
+var minTemp = -40;
+var maxTemp = 60;
+var tempScale = '°C';
+//
+//
+var tempLimit = 45;
+//
+
+//
+var minPres = 910;
+var maxPres = 1040;
+var presScale = 'hPa';
+//
+
+//
+//
+
+// 
+var mainTitle = 'Weather Station';
+var tempTitle = 'Temperature';
+var presTitle = 'Air Pressure';
+var tempWarning = 'Extrem Heat';
+var windTitle = 'Wind Speed';
+var windWarning = 'Storm Warning';
+// 
+// 
